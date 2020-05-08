d641c00260242c5ffac57505da6b91b33cba6d89
Allison Usher <allyjay317@gmail.com>
2020-05-07 19:56:17 -0700

    added event listeners for nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+let rotation = 45;
+let nav_links = document.querySelectorAll(".nav-link");
+nav_links.forEach(link =>{
+    link.style.transition = ""
+    link.addEventListener("mouseover", function(event)  {
+        this.style.transform = `rotate(${rotation}deg)`;
+        this.interval = setInterval(function(link) {
+            rotation = rotation * -1;
+            link.style.transform = `rotate(${rotation}deg)`;
+        }, 500, this);
+    })
+    link.addEventListener("mouseout", function(event) {
+        clearInterval(this.interval);
+        this.style.transform = "rotate(0deg)";
+    })
+})
+document.addEventListener("load", (event) => {
+    document.style.transition = "all fade-in 1s";
+}
+)
