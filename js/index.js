6de2047c91e5f129c039d50a465dba5d4db6be62
Allison Usher <allyjay317@gmail.com>
2020-05-08 19:48:19 -0700

    completed mvp

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 // Your code goes here
 
+
+
+
 let rotation = 45;
 let nav_links = document.querySelectorAll(".nav-link");
 nav_links.forEach(link =>{
+    
     link.style.transition = ""
     link.addEventListener("mouseover", function(event)  {
         this.style.transform = `rotate(${rotation}deg)`;
@@ -14,6 +18,14 @@ nav_links.forEach(link =>{
     link.addEventListener("mouseout", function(event) {
         clearInterval(this.interval);
         this.style.transform = "rotate(0deg)";
+    })
+    link.addEventListener("contextmenu", function(){
+        link.textContent += " With Feeling";
+
+    });
+    link.addEventListener("click", function(event){
+        event.preventDefault();
+        event.stopPropagation();
     })
 });
 
@@ -28,25 +40,34 @@ document.querySelector(".logo-heading").addEventListener("dblclick", function(ev
 });
 
 document.querySelector("body").addEventListener("click", function(event){
-    this.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
-
-});
-document.addEventListener("contextmenu", function(){
-
-});
-document.addEventListener("scroll", function(){
-
+    this.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
 });
 
-document.addEventListener("resize", function(){
 
+let lastY = window.scrollY;
+document.addEventListener("scroll", function(event){
+    let navBar = document.querySelector(".main-navigation");
+    if(lastY < window.scrollY){
+        navBar.style.height = `${navBar.clientHeight + 5}px`;
+    }
+    else{
+        navBar.style.height = `${navBar.clientHeight - 5}px`;
+    }
+    lastY = window.scrollY;
 });
-document.addEventListener("keydown", function(){
 
+window.addEventListener("resize", function(event){
+    document.querySelector(".logo-heading").textContent = "OH GOD THE WALLS ARE CLOSING IN!!!"
 });
-document.addEventListener("keyup", function(){
 
+window.addEventListener("keydown", function(event){
+    let funBus = document.querySelector(".home").querySelector("img");
+    funBus.setAttribute("src", "img/cab-calloway-jumpin-jive-nicholas-brothers-1.gif");
 });
-document.addEventListener("load", function(){
-
+document.addEventListener("keyup", function(event){
+    let funBus = document.querySelector(".home").querySelector("img");
+    funBus.setAttribute("src", "img/fun-bus.jpg");
+});
+window.addEventListener("load", function(event){
+    this.alert("The Window has Loaded")
 });
